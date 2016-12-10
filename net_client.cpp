5f32b017af1724e037654c89d7dc5cfaa82023ee
c501506022 <c501506022@tokushima-u.ac.jp>
2016-12-10 14:21:57 +0900

    pc094

--- net_client.cpp
+++ net_client.cpp
@@ -96,7 +96,7 @@ void client_start(void){
 	 u_short port = PORT;
 	 char server_name[MAX_LEN_NAME];
 
-	 sprintf(server_name,"clpc019");
+	 sprintf(server_name,"clpc094");
 
 	 setup_client(server_name,port);
 }
