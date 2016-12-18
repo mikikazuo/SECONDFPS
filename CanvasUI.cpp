d692307eff210e1dae5a051b5f67bd9e3625b8a8
c501506026 <c501506026@clpc105>
2016-12-18 15:59:56 +0900

    拠点HP表示調整
    画像の調整
    ゲージの空の部分を黒に

--- CanvasUI.cpp
+++ CanvasUI.cpp
@@ -45,10 +45,10 @@ void CanvasUI::DrawInitialize(){
 	handle[6]=image_Load("Data/image/exp_bar.png");
 
 	//HPbar 10-13
-	handle[10]=image_Load("Data/image/HP_bar_back.png");//下地B
-	handle[11]=image_Load("Data/image/HP_bar_back_Red.png");//下地R
-	handle[12]=image_Load("Data/image/HP_bar_tex.png");//質感
-	handle[13]=image_Load("Data/image/HP_bar_frame.png");//枠
+	handle[10]=image_Load("Data/image/HP_bar_B.png");//Blue
+	handle[11]=image_Load("Data/image/HP_bar_R.png");//Red
+	//handle[12]=image_Load("Data/image/HP_bar_tex.png");//質感
+	//handle[13]=image_Load("Data/image/HP_bar_frame.png");//枠
 
 	//Wallinfo
 	handle[14]=image_Load("Data/image/wall_info.png");//壁情報
@@ -134,19 +134,23 @@ void CanvasUI::Draw() {
 	glColor3d(1.0,0.2,0.2);//赤指定
 	if((time-dam)==0) glColor3d(1.0,1.0,1.0);
 	rect_Draw2D(handle[11],10+shakeX,10+shakeY,280*0.01*a,60);//ゲージ描画
-	image_DrawExRota(handle[11],150+shakeX,50+shakeY,0,1);//背景
+	glColor3d(0.2,0.2,0.2);//黒指定
+	rect_Draw2D(handle[11],10+shakeX+(280*0.01*a),10+shakeY,280*(1-0.01*a),60);//裏ゲージ描画
+	image_DrawExRota(handle[11],150+shakeX,50+shakeY,0,1);//HPバー
 
-	image_DrawExRota(handle[12],150+shakeX,50+shakeY,0,1);//ハイライト,質感
-	image_DrawExRota(handle[13],150+shakeX,50+shakeY,0,1);//枠
+	//image_DrawExRota(handle[12],150+shakeX,50+shakeY,0,1);//ハイライト,質感
+	//image_DrawExRota(handle[13],150+shakeX,50+shakeY,0,1);//枠
 
 	//敵陣HP
 	glColor3d(0.2,0.2,1.0);//青指定
 	if((time-dam)==0) glColor3d(1.0,1.0,1.0);
 	rect_Draw2D(handle[9],1190-280*0.01*a+shakeX,10+shakeY,280*0.01*a,60);//ゲージ描画
-	image_DrawExRota(handle[10],1050+shakeX,50+shakeY,0,1);//背景
+	glColor3d(0.2,0.2,0.2);//黒指定
+	rect_Draw2D(handle[9],910+shakeX,10+shakeY,280*(1-0.01*a),60);//裏ゲージ描画
+	image_DrawExRota(handle[10],1050+shakeX,50+shakeY,0,1);//HPバー
 
-	image_DrawExRota(handle[12],1050+shakeX,50+shakeY,0,1);//ハイライト、質感
-	image_DrawExRota(handle[13],1050+shakeX,50+shakeY,0,1);//枠
+	//image_DrawExRota(handle[12],1050+shakeX,50+shakeY,0,1);//ハイライト、質感
+	//image_DrawExRota(handle[13],1050+shakeX,50+shakeY,0,1);//枠
 
 
 	//レベル表示
