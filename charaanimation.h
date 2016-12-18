354316537f9243b310de030b3ae631df75bdcc2c
c501506022 <c501506022@tokushima-u.ac.jp>
2016-12-18 18:14:13 +0900

    データ

---
+++ charaanimation.h
@@ -0,0 +1,22 @@
+/*
+ * charaanimation.h
+ *
+ *  Created on: 2016/12/16
+ *      Author: c501506022
+ */
+
+#ifndef CHARAANIMATION_H_
+#define CHARAANIMATION_H_
+
+class chara_animation {
+public:
+	chara_animation();
+    void Initialize();			//初期化処理をオーバーライド。
+    void Update() ;				//更新処理をオーバーライド。
+    void DrawInitialize();
+    void Draw() ;					//描画処理をオーバーライド。
+    void DrawFinalize();
+	virtual ~chara_animation();
+};
+
+#endif /* CHARAANIMATION_H_ */
---
+++ get_sdata.h
@@ -0,0 +1,17 @@
+/*
+ * get_sdata.h
+ *
+ *  Created on: 2016/12/13
+ *      Author: c501506022
+ */
+
+#ifndef GET_SDATA_H_
+#define GET_SDATA_H_
+
+
+extern int get_bulletdata(S_CONTAINER);
+extern int get_playerdata(S_CONTAINER);
+int get_MapData(S_CONTAINER sdata);
+int get_MobData(S_CONTAINER sdata);
+
+#endif /* GET_SDATA_H_ */
