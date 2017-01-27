4e3f4820f3412c63b86142352c9c1bb277fdea61
c501506026 <c501506026@clpc007>
2017-01-27 14:18:02 +0900

    いちじこみっと

--- charaanimation.cpp
+++ charaanimation.cpp
@@ -50,6 +50,12 @@ void chara_animation::DrawInitialize(){
 	char2[5]=mqoCreateModel(flname,0.0035);
 	flname=(char*)"Data/charamodel/char2/char2_ene_leg3.mqo";
 	char2[6]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char2/char2_ene_shoot.mqo";
+	char2[7]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char2/char2_ene_shooted.mqo";
+	char2[8]=mqoCreateModel(flname,0.0035);
+
+
 
 	flname=(char*)"Data/charamodel/char1/char1_ene_body.mqo";
 	char1[0]=mqoCreateModel(flname,0.0035);
@@ -67,6 +73,10 @@ void chara_animation::DrawInitialize(){
 	char1[6]=mqoCreateModel(flname,0.0035);
 	flname=(char*)"Data/charamodel/char1/char1_ene_head.mqo";
 	char1[7]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char1/char1_ene_shoot.mqo";
+	char1[8]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char1/char1_ene_shooted.mqo";
+	char1[9]=mqoCreateModel(flname,0.0035);
 
 
 	flname=(char*)"Data/charamodel/char3/char3_ene_body.mqo";
@@ -83,6 +93,11 @@ void chara_animation::DrawInitialize(){
 	char3[5]=mqoCreateModel(flname,0.0035);
 	flname=(char*)"Data/charamodel/char3/char3_ene_head.mqo";
 	char3[6]=mqoCreateModel(flname,0.0035);
+	//flname=(char*)"Data/charamodel/char3/char3_ene_shoot.mqo";
+	//char3[7]=mqoCreateModel(flname,0.0035);
+	//flname=(char*)"Data/charamodel/char3/char3_ene_shooted.mqo";
+	//char3[8]=mqoCreateModel(flname,0.0035);
+
 
 	flname=(char*)"Data/charamodel/char4/char4_ene_body1.mqo";
 	char4[0]=mqoCreateModel(flname,0.0035);
@@ -98,6 +113,10 @@ void chara_animation::DrawInitialize(){
 	char4[5]=mqoCreateModel(flname,0.0035);
 	flname=(char*)"Data/charamodel/char4/char4_ene_head.mqo";
 	char4[6]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char4/char4_ene_arm_shoot.mqo";
+	char4[7]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char4/char4_ene_arm_shooted.mqo";
+	char4[8]=mqoCreateModel(flname,0.0035);
 
 	flname=(char*)"Data/charamodel/char5/char5_ene_body.mqo";
 	char5[0]=mqoCreateModel(flname,0.0035);
@@ -115,6 +134,9 @@ void chara_animation::DrawInitialize(){
 	char5[6]=mqoCreateModel(flname,0.0035);
 	flname=(char*)"Data/charamodel/char5/char5_ene_head.mqo";
 	char5[7]=mqoCreateModel(flname,0.0035);
+	flname=(char*)"Data/charamodel/char5/char5_ene_arm_shoot.mqo";
+	char5[8]=mqoCreateModel(flname,0.0035);
+
 
 	flname=(char*)"Data/charamodel/char6/char6_ene_body.mqo";
 	char6[0]=mqoCreateModel(flname,0.0035);
@@ -276,5 +298,90 @@ void walk(int model_no,double x,double y,double z){
 	default:break;
 	}
 
+}
+
+//ニュートラルな腕
+void arm(int model_no,double x,double y,double z){
+	//歩行用アニメーション番号　no
+	int no = 0;
+	if(time%60<15) no = 0;
+	else if(time%60<30) no = 1;
+	else if(time%60<45) no = 0;
+	else no = 2;
+
+	double leg = y + 0.5;
+	double arm = leg;
+
+	switch(model_no){
+		case 1:
+			arm = leg+0.9;
+			modelPP(char1[4],x,arm,z);//arm
+			break;
+		case 2:
+			arm = leg+0.7;
+			modelPP(char2[4+no],x,leg,z);//leg
+			modelPP(char2[2],x,head,z);//head
+			modelPP(char2[3],x,arm,z);//arm
+			break;
+		case 3:
+			body = leg+0.7;
+			arm = leg+0.9;
+			head = leg+1.2;
+			modelPP(char3[0],x,body,z);//body
+			modelPP(char3[1+no],x,leg,z);//leg
+			modelPP(char3[6],x,head,z);//head
+			modelPP(char3[4],x,arm,z+0.2);//arm 腕がもげるのでｚも補正
+			break;
+		case 4:
+			body = leg+0.5;
+			arm = leg+0.7;
+			head = leg+1.1;
+			modelPP(char4[0],x,body,z);//body
+			modelPP(char4[1+no],x,leg,z);//leg
+			modelPP(char4[6],x,head,z);//head
+			modelPP(char4[4],x,arm,z);//arm
+			break;
+		case 5:
+			body = leg+0.5;
+			arm = leg+0.6;
+			head = leg+1.3;
+			modelPP(char5[0],x,body,z);//body
+			modelPP(char5[1+no],x,leg,z);//leg
+			modelPP(char5[7],x,head,z);//head
+			modelPP(char5[4+no],x,arm,z);//arm
+			break;
+		case 6:
+			body = leg+0.4;
+			arm = leg+0.7;
+			head = leg+1.4;
+			modelPP(char6[2],x,body,z);//body
+			modelPP(char6[3+no%2],x,leg,z);//leg
+			modelPP(char6[5],x,head,z);//head
+			modelPP(char6[6],x,arm,z);//arm
+			break;
+		default:break;
+		}
+}
+
+
+//ショット用(腕だけ)
+void shot_arm(int model_no,double x,double y,double z){
+	static int start = 0;//アニメーション開始時間
+	static int anim = 0;//アニメーション中かどうか
+
+	if(anim == 0){
+		start = time;
+		anim = 1;
+	}
+
+	if(anim == 1){
+		if((time-start)>60){//60秒後ならアニメ終わり
+			anim = 0;
+		}else if((time-start)>0 && (time-start)<=30){//30秒後まで
+
+		}else if((time-start)>30 && (time-start)<=60){//30-60病後
+
+		}
+	}
 }
 
