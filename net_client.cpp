7a356f4f6591bd7a189b2aeb96a8ad128d7d9ea1
c501506019 <c501506019@clpc018>
2017-02-15 12:57:35 +0900

    とりま

--- net_client.cpp
+++ net_client.cpp
@@ -106,7 +106,7 @@ void client_start(void){
 	u_short port = PORT;
 	char server_name[MAX_LEN_NAME];
 
-	sprintf(server_name,"clpc097");
+	sprintf(server_name,"clpc045");
 
 	setup_client(server_name,port);
 }
